60be34d19da09ac54c94305baab47428a6430283
Yeray Martinez <yeray.martinez@opendeusto.es>
2025-10-02 20:14:25 +0200

    ResamplingStrategy docs

--- komodoml/resampling/base.py
+++ komodoml/resampling/base.py
@@ -1,8 +1,25 @@
+from komodoml.base.base_model import BaseModel
+
+
 class ResamplingStrategy:
     """
     Base class for resampling-based training strategies.
     All resampling strategies should inherit from this class and implement the `fit` method.
     """
 
-    def fit(self, model, X, y, **kwargs):
+    def fit(self, model: BaseModel, X, y, **kwargs):
+        """
+        Fit the model using the resampling strategy.
+
+        Parameters
+        ----------
+        model : BaseModel
+            The model to fit.
+        X : array-like, shape (n_samples, n_features)
+            Training data.
+        y : array-like, shape (n_samples,) or (n_samples, n_outputs), default=None
+            Target values.
+        **kwargs : additional keyword arguments
+            Additional arguments passed to the model's fit method.
+        """
         raise NotImplementedError
