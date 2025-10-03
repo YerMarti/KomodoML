0429f0f73b64e1bc6964c218634616d5b0e9fc2d
Yeray Martinez <yeray.martinez@opendeusto.es>
2025-10-03 11:03:12 +0200

    Bootstrapping strategy added

--- komodoml/resampling/__init__.py
+++ komodoml/resampling/__init__.py
@@ -1,9 +1,11 @@
 from .base import ResamplingStrategy
 from .kfold import KFoldFit, StratifiedKFoldFit, LeaveOneOutFit
+from .bootstrap import BootstrapFit
 
 __all__ = [
     "ResamplingStrategy",
     "KFoldFit",
     "StratifiedKFoldFit",
-    "LeaveOneOutFit"
+    "LeaveOneOutFit",
+    "BootstrapFit"
 ]
