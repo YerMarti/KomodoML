7127a03833194c25133f13f50ac7c05b28e931e1
Yeray Martinez <yeray.martinez@opendeusto.es>
2025-10-04 12:07:55 +0200

    Version updated to "0.0.0.post1"

--- komodoml/__init__.py
+++ komodoml/__init__.py
@@ -2,7 +2,7 @@
 A unified ML toolkit for models, metrics, plotting, and interpretability.
 """
 
-__version__ = "0.0.0"
+__version__ = "0.0.0.post1"
 
 # Public API: only user-facing modules
 from . import models
